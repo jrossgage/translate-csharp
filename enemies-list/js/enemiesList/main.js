f3cb786bec9506e4774deb0a40e518200c01b028
Joel Gage <jrossgage@gmail.com>
2021-05-24 14:23:42 -0500

    first commit

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,1 +1,58 @@
 // Put your code here
+
+console.log("My Enemies List!");
+console.log("----------------");
+
+const enemiesArr =
+    [
+        {
+            firstName: "Joshua",
+            lastName: "Flowers",
+            offenses: [
+                "Being a jerk to me in elementary school",
+                "Not being nice to me in elementary school"
+            ],
+            isReallyHated: true
+        },
+        {
+            firstName: "Darth",
+            lastName: "Vader",
+            offenses: [
+                "Cut off Luke's hand",
+                "Murdered all those kids",
+                "Unkind management practices"
+            ],
+            isReallyHated: false
+        },
+        {
+            firstName: "Betty",
+            lastName: "Rudelady",
+            offenses: [
+                "Phone calls in the theater",
+                "Phone calls on the bus",
+                "Phone calls in line at the grocery store",
+                "Poor cnversationalist"
+            ],
+            isReallyHated: true
+        },
+        {
+            firstName: "Leon",
+            lastName: "Peck",
+            offenses: [
+                "Keeps giving me a hotplate"
+            ],
+            isReallyHated: false
+        }
+    ]
+
+const displayEnemies = () => {
+    for (enemy of enemiesArr) {
+        if (enemy.isReallyHated === true) {
+            console.log(`${enemy.firstName} ${enemy.lastName} (Really, really dislike!)`)
+        } else {
+            console.log(`${enemy.firstName} ${enemy.lastName}`)
+        }
+    }
+}
+
+displayEnemies();
--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,19 @@
 // Put your code here
+
+const locationNamesArr = ["Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
+
+console.log("All Place Names")
+
+for (let name of locationNamesArr) {
+    console.log(name)
+}
+
+console.log("")
+
+const filteredLocationArr = locationNamesArr.filter(location => location.startsWith("The"))
+
+console.log("'The' Place Names")
+for (let name of filteredLocationArr) {
+    console.log(name)
+}
